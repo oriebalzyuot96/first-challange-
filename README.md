64d7d8fc11eff95812a05d95f860a17295967585
oriebalzyuot96 <=>
2020-06-18 16:18:30 +0300

    handle event

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ to build table :
 *W3SCHOOL
 *https://stackoverflow.com/questions/21086585/html-layout-issue-3x3-table-with-centered-image
 *https://stackoverflow.com/questions/12922027/create-table-using-div-and-css
+/////////////
+*event lisnner 
+*https://stackoverflow.com/questions/20772954/add-click-event-on-div-tag-using-javascript
+*https://stackoverflow.com/questions/54508019/click-on-multiple-elements-same-class-name
--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 //console.log("you can do")
 //build table ...using some referance i mintioned in readme ...and make styling ..
 //so i build 1st design 
+//add event for each square in board(cell)
+
+//problem:work to one cell
+//document.querySelector(".cell1").addEventListener('click',function(){
+// 	console.log("orieb")
+// },false)
+
+// document.getElementsByClassName('cell2')[0]
+//         .addEventListener('click', function (event) {
+//             // do something
+//             console.log("cell2")
+//         });
+
+var clk = document.getElementsByClassName('cell');
+
+Array.prototype.forEach.call(clk, function(element) {
+  element.addEventListener('click', function() {
+    console.log('data-wow value is: ' + element.dataset.wow);
+  });
+});
